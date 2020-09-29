207e7e01402b9fcdf2185507e83ee5bc034b1ead
chensenhua <761063381@qq.cpm>
2020-09-29 17:54:06 +0800

    t

--- FileDemo/FileDemo/ViewController.swift
+++ FileDemo/FileDemo/ViewController.swift
@@ -6,12 +6,40 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let fileManager = FileManager.default;
+        let documentUrl = try?fileManager.url(for: .documentDirectory, in: .allDomainsMask, appropriateFor: nil, create: false).absoluteString;
+        printChildUrl(path: documentUrl!)
+        
+        let myDirectory:String = NSHomeDirectory() + "/Documents/myFolder/Files"
+        do{
+            try  fileManager.createDirectory(atPath: myDirectory, withIntermediateDirectories: true, attributes: nil)
+        }catch  {
+            print("create dir failed")
+        }
+        
+        printChildUrl(path: NSHomeDirectory() )
+            
+            
+            
+        
+    }
+    
+    
+    func printChildUrl(path:String)  {
+        let fileManager = FileManager.default;
+         print("path : \(path)")
+        fileManager.enumerator(atPath: path)?.forEach{
+            children in
+            print("document child url:\(children)")
+        }
     }
 
 
